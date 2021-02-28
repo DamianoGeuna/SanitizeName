711218e7cf9bd48445bc65c76c21818199b6e4cc
DamianoGeuna <damigeuna@gmail.com>
2021-02-28 22:15:07 +0100

    Seconda Correzione

--- sanitizeName.php
+++ sanitizeName.php
@@ -2,17 +2,29 @@
 
 function sanitizeName($name){
     
-    $finalspace= preg_replace('/[^a-zA-Z]+$/','',$name);
-    $tinyName = preg_replace('/[^a-zA-Z ]+/','',$finalspace);
-    $onespace = preg_replace('/[ ]+/',' ',$tinyName);
-    $notags = trim($onespace,"<h1>");
+    //$noScript = preg_replace('/<script\b[^>]*>(.*?)<\/script>/m', "", $name);
 
-    $explodeName = explode(" ",$notags);
-    $correctNames = array_map('correctCase', $explodeName);
-    
-    return implode(" ", $correctNames); // si potrebbe mettere direttamente il return eseguito dalla variabile. $implodeName;
-    
-    //return$uppercaseName;
+    $noSpaceEndName = preg_replace('/(\s)+$/',"",$name);
+    $noSpaceStartName = preg_replace('/(^\s*)+/',"",$noSpaceEndName);
+
+    //elimina tag
+    $noTagName = strip_tags($noSpaceStartName);
+
+    //$strippedName = filter_var($noSpaceName, FILTER_SANITIZE_STRING);
+
+    //elimina simboli e numeri
+    $skinnyName = preg_replace('/[^a-zA-Z .]+/',"",$noTagName);
+    $cleanName = preg_replace('/[ ]{2,}/'," ",$skinnyName);
+
+    //rende stringa in array
+    $explodeName = explode(" ",$cleanName);
+    //corregge maiuscole e minuscole
+    $correctNames = array_map('correctCase',$explodeName);
+    //ricompatta array creato con explode e esce con stringa singola
+    $finalName = implode(" ",$correctNames);
+    $noSpaceEndNameLast = preg_replace('/(\s)+$/',"",$finalName);
+
+    return preg_replace('/(^\s*)+/',"",$noSpaceEndNameLast);
 
     
 }
